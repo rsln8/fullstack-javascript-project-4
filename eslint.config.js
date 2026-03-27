825a7e18c58e948766d983efa9e5de3b37eb1f8b
rsln8 <iamrsln23@my.com>
2026-03-27 20:50:26 +0300

    Исправить sourceType в eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+﻿export default [
+  {
+    files: ["**/*.js", "**/*.cjs"],
+    languageOptions: {
+      sourceType: "module",
+      ecmaVersion: "latest",
+      globals: {
+        node: true,
+        jest: true,
+      },
+    },
+    rules: {
+      "no-console": "off",
+      "no-underscore-dangle": "off",
+      "no-restricted-syntax": "off",
+      "linebreak-style": "off",
+      "import/no-extraneous-dependencies": "off",
+    },
+  },
+];
