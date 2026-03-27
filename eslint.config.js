93a798cbf7b9cedb08d27e6424dd4dd7702a472e
rsln8 <iamrsln23@my.com>
2026-03-27 20:14:49 +0300

    Настроил ESLint 9

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+﻿export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "module",
+      ecmaVersion: "latest",
+      globals: {
+        node: true,
+        jest: true,
+      },
+    },
+    rules: {
+      "no-console": "off",
+      "no-underscore-dangle": "off",
+      "no-restricted-syntax": "off",
+      "linebreak-style": "off",
+    },
+  },
+];
